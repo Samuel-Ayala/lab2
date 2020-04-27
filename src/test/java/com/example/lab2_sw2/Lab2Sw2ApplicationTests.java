ce106e6b8b2c7097696e4c38bcb7e94837c93e18
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-04-27 18:57:51 -0500

    primer push

---
+++ src/test/java/com/example/lab2_sw2/Lab2Sw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_sw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Sw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
