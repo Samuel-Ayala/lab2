03d25f1f137e101b8992fa0d74a9e96580074f15
Gabrielcv98 <gabrialbert98@gmail.com>
2020-04-27 20:30:10 -0500

    Lista de trabajos acabada

--- src/main/java/com/example/lab2_sw2/Repository/JobRepository.java
+++ src/main/java/com/example/lab2_sw2/Repository/JobRepository.java
@@ -2,8 +2,11 @@ package com.example.lab2_sw2.Repository;
 
 import com.example.lab2_sw2.Entity.JobsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface JobRepository extends JpaRepository<JobsEntity,Integer> {
+import java.util.List;
 
-
+public interface JobRepository extends JpaRepository<JobsEntity,String> {
+    @Query(value = "SELECT job_title from jobs where ?1 Between min_salary And max_salary", nativeQuery = true)
+    List<JobsEntity> EncontrarSalario(int salario);
 }
