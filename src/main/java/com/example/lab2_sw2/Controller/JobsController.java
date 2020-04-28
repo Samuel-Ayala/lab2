3c68489c0b4c74a36f090a29298d77f10f1621e7
Gabrielcv98 <gabrialbert98@gmail.com>
2020-04-27 19:47:12 -0500

    Controlador de Job sin terminar (falta Borrar)

--- src/main/java/com/example/lab2_sw2/Controller/JobsController.java
+++ src/main/java/com/example/lab2_sw2/Controller/JobsController.java
@@ -1,13 +1,60 @@
 package com.example.lab2_sw2.Controller;
 
 
+import com.example.lab2_sw2.Entity.JobsEntity;
+import com.example.lab2_sw2.Repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/job")
 
 
 public class JobsController {
+    @Autowired
+    JobRepository jobRepository;
+
+
+    @GetMapping(value = {"","/listar"})
+    public String listaJobs(Model model){
+        List<JobsEntity> listaJob = jobRepository.findAll();
+        model.addAttribute("lista",listaJob);
+        return "job/lista";
+
+
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevoJob(){
+        return "job/nuevoformulario";
+
+    }
+
+    @PostMapping("/guardar")
+    public String guardarJob(JobsEntity job){
+        jobRepository.save(job);
+        return "redirect:/job";
+    }
+
+    @GetMapping("/editar")
+    public String editarJobForm(@RequestParam("id") String id, Model model) {
+        Optional<JobsEntity> opt = jobRepository.findById(id);
+        if (opt.isPresent()) {
+            JobsEntity job = opt.get();
+            model.addAttribute("job", job);
+            return "job/editForm";
+        } else {
+            return "redirect:/job";
+
+        }
+    }
 
 }
--- src/main/java/com/example/lab2_sw2/Repository/JobRepository.java
+++ src/main/java/com/example/lab2_sw2/Repository/JobRepository.java
@@ -3,7 +3,7 @@ package com.example.lab2_sw2.Repository;
 import com.example.lab2_sw2.Entity.JobsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobRepository extends JpaRepository<JobsEntity,Integer> {
+public interface JobRepository extends JpaRepository<JobsEntity,String> {
 
 
 }
